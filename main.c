eb983a64cbaf81b2f8bcbfee7a5ee0408af4b226
Cys-Cys <90268890+Cys-Cys@users.noreply.github.com>
2024-03-14 00:04:59 +0800

    Update main.c

--- main.c
+++ main.c
@@ -26,28 +26,6 @@ int main(int argc, char * argv[]){
 	return 0;
 }
 
-void Mix_GetC_Scanf() {
-	/* 
-		针对getchar与scanf获取字符流输入问题
-		使用特性不同导致混用有一定难度
-	*/
-	int ch;
-	int a, b;
-
-	while ((ch = getchar()) != '\n') {
-		if (scanf_s("%d %d", &a, &b) == 2) {
-			printf("%c %d %d\n", ch, a, b);
-		}
-		else {
-			printf("warning, you cannot do this.Try again.\n");
-		}
-		
-		while (getchar() != '\n');  // 过滤多余的字符
-	}
-
-	return;
-}
-
 void Test_Bool() {
 	int num;
 	bool Test = scanf("%d", &num);
