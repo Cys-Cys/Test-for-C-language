50b0771b47f1887c27fb3d74a103b322ca72c701
Cys-Cys <90268890+Cys-Cys@users.noreply.github.com>
2024-03-13 20:15:28 +0800

    Update main.c

--- main.c
+++ main.c
@@ -29,23 +29,23 @@ void Test_Standard_io_Stream() {
 	* 一般gets与puts搭配；fgets与fputs搭配，常用于文件读写
 	*/
 
-  char cs[CHARS_NUM] = { 0 };
-  
-  printf("Now Test gets() function:\n");
-  gets(cs);  // 将不会检查输入是否会导致溢出，不安全；不会存储换行符号
-  puts(cs);  // 输出时会自动产生一个换行符
-  
-  printf("Now Test fgets() function:\n");
-  fgets(cs, CHARS_NUM, stdin); // 会接受CHARS_NUM - 1个字符，或者遇到回车符号；会存储换行符号；格式化输入
-  fputs(cs, stdout);  // 输出时不会自带换行符
-  
-  /* 如果要清除输入时的残留字符，针对于格式化输入，请使用以下参考代码
-  * while(getchar() != '\n')
-  *		continue;
-  */
-  
-  printf("Now Test End.");
-  return;
+	  char cs[CHARS_NUM] = { 0 };
+	  
+	  printf("Now Test gets() function:\n");
+	  gets(cs);  // 将不会检查输入是否会导致溢出，不安全；不会存储换行符号
+	  puts(cs);  // 输出时会自动产生一个换行符
+	  
+	  printf("Now Test fgets() function:\n");
+	  fgets(cs, CHARS_NUM, stdin); // 会接受CHARS_NUM - 1个字符，或者遇到回车符号；会存储换行符号；格式化输入
+	  fputs(cs, stdout);  // 输出时不会自带换行符
+	  
+	  /* 如果要清除输入时的残留字符，针对于格式化输入，请使用以下参考代码
+	  * while(getchar() != '\n')
+	  *		continue;
+	  */
+	  
+	  printf("Now Test End.");
+	  return;
 }
 
 void Mix_GetC_Scanf() {
@@ -90,19 +90,42 @@ void Test_Float() {
 
 void Test_Num()
 {
-		float a = 3.33;
-		unsigned long long s = 1;
-
-		printf("%s\n", Q);  // 输出带双引号的Q
-		printf("test " Q  "test" "\n");  // 分段字符输出
-		printf("%*.3f \n", 9, a);  // *号用于动态指定宽度设定
-		printf("%llu \n", s);
-		printf("%zd\n", sizeof(unsigned long long));
+	float a = 3.33;
+	unsigned long long s = 1;
+	
+	printf("%s\n", Q);  // 输出带双引号的Q
+	printf("test " Q  "test" "\n");  // 分段字符输出
+	printf("%*.3f \n", 9, a);  // *号用于动态指定宽度设定
+	printf("%llu \n", s);
+	printf("%zd\n", sizeof(unsigned long long));
 
     return;
 }
 
-void Test_Point(){
+void Test_Point_Const(const int array_outer[]){  // 此处的形参表明array_outer是个指针，同时提醒程序员一般是数组传入
+	int array[CHARS_NUM] = { 0 };
+	int* p1 = array;  // 大部分情况下两者使用共通
 
-  return;
+	int array1[CHARS_NUM][CHARS_NUM] = { 0 };
+	int*p2 = array1; // 赋值不可取
+	p2 = array1[0]; // 有效
+	
+	int**pp1 = array1;  // 该赋值也有问题
+	*pp1 = array1[0];  // 没问题
+	
+	int (*p3)[CHARS_NUM] = array1;  // 该赋值没问题，这是由单个指针构成，一次指向CHARS_NUM个数据
+	int *p4[CHARS_NUM] = array1;  // 注意两者区别，这是由指针构成的数组
+
+	// 注意：不要对多层引用加上const限定符，可能导致意外的结果
+	const int **pp2;
+	int *p5;
+	const int n = 13;
+	pp2 = &p5;
+	*pp2 = &n;
+	*p5 = 10;
+
+	const int const *p6 = p1;  // 不可更改值，也不能指向其余的地方；相应的const对应相应的要求
+
+	
+	return;
 }
